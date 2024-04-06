28630ae04f53bcac8ca49d9a3a1133659dfc7190
Arsenii Karpov <abs2016123@gmail.com>
2024-04-06 15:51:30 +0300

    admin for events

--- event_manager/events/admin.py
+++ event_manager/events/admin.py
@@ -1,1 +1,26 @@
-# Register your models here.
+from django.contrib import admin
+
+from events import models
+
+
+class ParticipantsInline(admin.TabularInline):
+    model = models.EventParticipants
+    can_delete = False
+    extra = 1
+    verbose_name = 'участник мероприятия'
+    verbose_name_plural = 'участники мероприятия'
+
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [ParticipantsInline]
+    list_display = (
+        models.Event.title.field.name,
+        models.Event.is_private.field.name,
+        models.Event.is_canceled.field.name,
+        models.Event.author.field.name,
+    )
+    list_editable = (
+        models.Event.is_private.field.name,
+        models.Event.is_canceled.field.name,
+    )
