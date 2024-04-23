6221bb4f81033d3eeadb8a692eea5556ca674cfe
Arsenii Karpov <abs2016123@gmail.com>
2024-04-23 20:02:43 +0300

    3

---
+++ event_manager/tgbot/bot.py
@@ -0,0 +1,29 @@
+import os
+
+import dotenv
+import requests
+import telebot
+
+dotenv.load_dotenv()
+
+bot = telebot.TeleBot(os.getenv('TG_TOKEN'))
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.reply_to(message, 'Please enter your username.')
+    bot.register_next_step_handler(message, save_username)
+
+def save_username(message):
+    username = message.text
+    chat_id = message.chat.id
+    response = requests.post('http://django:8000/notifications/link_telegram_user/', data={'chat_id': chat_id, 'username': username})
+    if response.status_code == 200:
+        bot.reply_to(message, 'You have been linked to your Django user account.')
+    else:
+        bot.reply_to(message, 'Failed to link your Telegram user to your Django user account.')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
