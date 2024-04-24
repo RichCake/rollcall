60ad8206454b2729459dac642d355830f88a648d
WizzNot <azaza22122004@gmail.com>
2024-04-24 18:44:14 +0500

    admin added avatar

--- event_manager/users/admin.py
+++ event_manager/users/admin.py
@@ -5,6 +5,12 @@ from django.utils.translation import gettext_lazy as _
 from users.models import User
 
 class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Avatar'), {'fields': ('avatar',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Avatar'), {'fields': ('avatar',)}),
+    )
     list_display = ('username', 'email', 'rating', 'is_staff')
 
 admin.site.register(User, CustomUserAdmin)
