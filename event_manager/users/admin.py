f1e03d60926b57e8c7634d6cb227d448587d9acb
WizzNot <azaza22122004@gmail.com>
2024-04-24 18:38:04 +0500

    changed templates

--- event_manager/users/admin.py
+++ event_manager/users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
-
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'rating', 'is_staff')
