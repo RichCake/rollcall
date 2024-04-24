ff09245ae74d44a8722e620b6acfd139a7c8834b
WizzNot <azaza22122004@gmail.com>
2024-04-24 18:53:03 +0500

    sorted imports

--- event_manager/users/admin.py
+++ event_manager/users/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (_('Avatar'), {'fields': ('avatar',)}),
